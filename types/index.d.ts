511c990042447b34a80b2986288ffc109242a739
Christoph Kruppe <c.kruppe@nfq.de>
2023-03-05 18:10:50 +0100

    fix(Ignore): Ignore files not needed

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,34 @@
-interface FontItem {
+export interface FontItem {
     fontDisplay: 'auto' | 'block' | 'fallback' | 'optional' | 'swap';
     fontStyle: 'italic' | 'normal' | 'oblique' | `oblique ${number}deg ${number}deg` | `oblique ${number}deg`;
     fontWeight: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900' | 'bold' | 'normal';
     preload?: boolean;
     src: `/fonts/${string}.${string}`[] | `/fonts/${string}.${string}`;
 }
-export type FontDefinition = Record<string, FontItem[]>;
+export type FontDefinition<Type extends Record<string, FontItem[]>> = {
+    [Property in keyof Type]: FontItem[];
+};
+export type FontList<Type> = {
+    [Property in keyof Type]: `'${string}', Arial, sans-serif`;
+};
+/**
+ * Configuration Helper for fonts.
+ *
+ * @param config The font configuration.
+ * @returns The font configuration.
+ */
+export declare const configureFonts: <T extends FontDefinition<T>>(config: T) => T;
+/**
+ * Gets the font theme.
+ *
+ * @param config The font configuration.
+ * @returns The font theme.
+ */
+export declare const getFontThemeList: <T extends FontDefinition<T>>(config: T) => FontList<T>;
 /**
  * Preloads fonts.
  *
  * @param fontDefinitions The font definitions.
  * @returns An array of font links.
  */
-export declare const preloadFonts: (fontDefinitions: FontDefinition) => JSX.Element[];
-export {};
+export declare const preloadFonts: (fontDefinitions: FontDefinition<Record<string, FontItem[]>>) => JSX.Element[];
