f1647ca99d0bb3d5902c9e3348f14c67caf2fe0b
Christoph Kruppe <c.kruppe@nfq.de>
2023-05-02 11:34:26 +0200

    fix(Webpack): Fix missing option (#7)

--- types/webpack/WatchFontsConfigPlugin.d.ts
+++ types/webpack/WatchFontsConfigPlugin.d.ts
@@ -3,6 +3,18 @@ import type { Compiler } from 'webpack';
  * @file
  */
 export declare class WatchFontsConfigPlugin {
+    readonly options: {
+        globalStylesPath?: string;
+    };
+    /**
+     * Constructor.
+     *
+     * @param options                  The options.
+     * @param options.globalStylesPath The global styles path.
+     */
+    constructor(options: {
+        globalStylesPath?: string;
+    });
     /**
      * Constructor.
      *
