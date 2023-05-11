1b62b92e4ec85640a7fbf931b4a9271a6a41e0bc
Christoph Kruppe <c.kruppe@nfq.de>
2023-05-11 13:34:20 +0200

    feat(Webpack): Add option for the css output path

--- src/webpack/WatchFontsConfigPlugin.ts
+++ src/webpack/WatchFontsConfigPlugin.ts
@@ -11,8 +11,9 @@ export class WatchFontsConfigPlugin {
      *
      * @param options                  The options.
      * @param options.globalStylesPath The global styles path.
+     * @param options.outputPath       The css file output path.
      */
-    constructor(readonly options: {globalStylesPath?: string}) {}
+    constructor(readonly options: {globalStylesPath?: string; outputPath?: string}) {}
 
     /**
      * Constructor.
@@ -21,7 +22,7 @@ export class WatchFontsConfigPlugin {
      */
     apply(compiler: Compiler) {
         compiler.hooks.afterPlugins.tap('WatchFontsConfigPlugin', () => {
-            const generator = new FontCSSGenerator(this.options.globalStylesPath);
+            const generator = new FontCSSGenerator(this.options.globalStylesPath, this.options.outputPath);
 
             generator.generate();
         });
@@ -38,7 +39,7 @@ export class WatchFontsConfigPlugin {
             }
 
             if (changedFile.includes('globalStyles')) {
-                const generator = new FontCSSGenerator(this.options.globalStylesPath);
+                const generator = new FontCSSGenerator(this.options.globalStylesPath, this.options.outputPath);
 
                 generator.generate();
             }
