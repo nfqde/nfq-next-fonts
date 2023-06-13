01ef3b122213a59b6ff8401a155785fa073d5685
Christoph Kruppe <c.kruppe@nfq.de>
2023-06-13 11:18:37 +0200

    fix(Module): Update packages (#10)

--- types/webpack/WatchFontsConfigPlugin.d.ts
+++ types/webpack/WatchFontsConfigPlugin.d.ts
@@ -5,15 +5,18 @@ import type { Compiler } from 'webpack';
 export declare class WatchFontsConfigPlugin {
     readonly options: {
         globalStylesPath?: string;
+        outputPath?: string;
     };
     /**
      * Constructor.
      *
      * @param options                  The options.
      * @param options.globalStylesPath The global styles path.
+     * @param options.outputPath       The css file output path.
      */
     constructor(options: {
         globalStylesPath?: string;
+        outputPath?: string;
     });
     /**
      * Constructor.
