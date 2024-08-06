3e045676626cebfc0ce03a948d4715a50995f25a
Christoph Kruppe <c.kruppe@nfq.de>
2024-08-06 11:20:10 +0200

    feat(prefix): Add prefix for css geneartion and activ enextjs base path (#18)

--- src/webpack/WatchFontsConfigPlugin.ts
+++ src/webpack/WatchFontsConfigPlugin.ts
@@ -12,8 +12,9 @@ export class WatchFontsConfigPlugin {
      * @param options                  The options.
      * @param options.globalStylesPath The global styles path.
      * @param options.outputPath       The css file output path.
+     * @param options.prefix           The font path prefix.
      */
-    constructor(readonly options: {globalStylesPath?: string; outputPath?: string}) {}
+    constructor(readonly options: {globalStylesPath?: string; outputPath?: string; prefix?: string}) {}
 
     /**
      * Constructor.
@@ -22,7 +23,11 @@ export class WatchFontsConfigPlugin {
      */
     apply(compiler: Compiler) {
         compiler.hooks.afterPlugins.tap('WatchFontsConfigPlugin', () => {
-            const generator = new FontCSSGenerator(this.options.globalStylesPath, this.options.outputPath);
+            const generator = new FontCSSGenerator(
+                this.options.globalStylesPath,
+                this.options.outputPath,
+                this.options.prefix
+            );
 
             generator.generate();
         });
@@ -39,7 +44,11 @@ export class WatchFontsConfigPlugin {
             }
 
             if (changedFile.includes('globalStyles')) {
-                const generator = new FontCSSGenerator(this.options.globalStylesPath, this.options.outputPath);
+                const generator = new FontCSSGenerator(
+                    this.options.globalStylesPath,
+                    this.options.outputPath,
+                    this.options.prefix
+                );
 
                 generator.generate();
             }
